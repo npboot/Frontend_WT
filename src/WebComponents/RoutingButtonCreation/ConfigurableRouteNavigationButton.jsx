f2ae3514b3ec28e57968e9f14784f2785aacfea1
LegacyRevida <76050148+LeslieCrielaard@users.noreply.github.com>
2024-09-10 09:32:07 +0200

    added regular configurable routing button to project

--- src/WebComponents/RoutingButtonCreation/ConfigurableRouteNavigationButton.jsx
+++ src/WebComponents/RoutingButtonCreation/ConfigurableRouteNavigationButton.jsx
@@ -2,12 +2,23 @@ import Button from '@mui/material/Button';
 import { useNavigate } from "react-router";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-export function ConfigurableRouteNavigationBT({route,text}) {
+//Button created without border or fill, shows a back arrow with text related to the route
+export function ConfigurableRouteNavigationBTNoFill({route,text}) {
     const navigate = useNavigate();
     const SetNavigate = () => {
         navigate(route);
     };
     return (
         <Button style={{marginLeft:"5%", paddingTop:"1%", paddingBottom:"1%"}} onClick={()=>{SetNavigate()}}><ArrowBackIcon/> {text}</Button>
+    );
+  }
+
+  export function ConfigurableRouteNavigationBTFill({route,text}) {
+    const navigate = useNavigate();
+    const SetNavigate = () => {
+        navigate(route);
+    };
+    return (
+  <Button onClick={()=>{SetNavigate()}}>{text}</Button>
     );
   }
